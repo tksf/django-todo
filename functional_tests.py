ea968202f046f29d97a7fc8cea1e59f139fd0a3f
T K <kytomaa@gmail.com>
2015-01-23 13:29:36 -0800

    added unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,9 +1,25 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
 
-# cool new online to-do app
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-# title and headers mentions to-do list
-assert 'To-Do' in browser.title
+	def setUp(self):
+		self.browser = webdriver.Firefox()
+		self.browser.implicitly_wait(3)
+
+	def tearDown(self):
+		self.browser.quit()
+
+	def test_can_start_a_list_and_retrieve_it_later(self):
+
+		# cool new online to-do app
+		self.browser.get('http://localhost:8000')
+
+		# title and headers mentions to-do list
+		self.assertIn('To-Do', self.browser.title)
+		self.fail('Finish the test!')
+
+if __name__ == '__main__':
+	unittest.main(warnings='ignore')
+
