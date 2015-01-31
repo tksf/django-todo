5364f2cd994f6c2c0bc1841fbb40b053a6497367
T K <kytomaa@gmail.com>
2015-01-30 16:47:16 -0800

    refactoring and cleaning up

--- lists/tests.py
+++ lists/tests.py
@@ -22,41 +22,11 @@ class HomePageTest(TestCase):
 		self.assertEqual(response.content.decode(), expected_html)
 
 
-	def test_home_page_can_save_a_POST_request(self):
-		request = HttpRequest()
-		request.method = 'POST'
-		request.POST['item_text'] = 'A new list item'
+	# def test_home_page_only_saves_items_when_necessary(self):
+	# 	request = HttpRequest()
+	# 	response = home_page(request)
 
-		response = home_page(request)
-
-		self.assertEqual(Item.objects.count(), 1)
-		new_item = Item.objects.first()
-		self.assertEqual(new_item.text, 'A new list item')
-
-	def test_home_page_redicrects_after_POST(self):
-		request = HttpRequest()
-		request.method = 'POST'
-		request.POST['item_text'] = 'A new list item'
-
-		response = home_page(request)
-
-		self.assertEqual(response.status_code, 302)
-		self.assertEqual(response['location'], '/lists/the-only-list-in-the-world')
-
-		# self.assertIn('A new list item', response.content.decode())
-		# expected_html = render_to_string(
-		# 	'home.html',
-		# 	{'new_item_text' : 'A new list item'}
-		# )
-
-		# self.assertIn('A new list item', response.content.decode())
-
-
-	def test_home_page_only_saves_items_when_necessary(self):
-		request = HttpRequest()
-		response = home_page(request)
-
-		self.assertEqual(Item.objects.count(), 0)
+	# 	self.assertEqual(Item.objects.count(), 0)
 
 
 
@@ -85,7 +55,7 @@ class ListViewTest(TestCase):
 	def test_uses_list_template(self):
 		response = self.client.get('/lists/the-only-list-in-the-world/')
 		self.assertTemplateUsed(response, 'list.html')
-		
+
 
 	def test_displays_all_items(self):
 		Item.objects.create(text='itemey 1')
@@ -97,9 +67,46 @@ class ListViewTest(TestCase):
 		self.assertContains(response, 'itemey 2')
 
 
+class NewListTest(TestCase):
 
-# class SmokeTest(TestCase):
+	def test_saving_a_POST_request(self):
+		# request = HttpRequest()
+		# request.method = 'POST'
+		# request.POST['item_text'] = 'A new list item'
 
-# 	def test_bad_maths(self):
-# 		self.assertEqual(1 * 1, 3)
+		# response = home_page(request)
+
+		self.client.post(
+			'/lists/new',
+			data={'item_text' : 'A new list item'}
+		)
+
+		self.assertEqual(Item.objects.count(), 1)
+		new_item = Item.objects.first()
+		self.assertEqual(new_item.text, 'A new list item')
+
+
+	def test_redicrects_after_POST(self):
+		# request = HttpRequest()
+		# request.method = 'POST'
+		# request.POST['item_text'] = 'A new list item'
+
+		# response = home_page(request)
+
+		response = self.client.post(
+			'/lists/new',
+			data={'item_text' : 'A new list item'}
+		)
+
+		self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
+
+
+		# self.assertEqual(response.status_code, 302)
+		# self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
+
+
+
+
+
+
 
--- lists/views.py
+++ lists/views.py
@@ -5,11 +5,11 @@ from lists.models import Item
 
 def home_page(request):
 
-	if request.method == 'POST':
-		# new_item_text = request.POST['item_text']
-		# Item.objects.create(text=new_item_text)
-		Item.objects.create(text=request.POST['item_text'])
-		return redirect('/lists/the-only-list-in-the-world')
+	# if request.method == 'POST':
+	# 	# new_item_text = request.POST['item_text']
+	# 	# Item.objects.create(text=new_item_text)
+	# 	Item.objects.create(text=request.POST['item_text'])
+	# 	return redirect('/lists/the-only-list-in-the-world')
 
 	# items = Item.objects.all()
 	return render(request, 'home.html')
@@ -18,6 +18,13 @@ def home_page(request):
 def view_list(request):
 	items = Item.objects.all()
 	return render(request, 'list.html', {'items' : items})
+
+
+def new_list(request):
+
+	Item.objects.create(text=request.POST['item_text'])
+	return redirect('/lists/the-only-list-in-the-world/')
+
 
 
 	# else:
