4daec4509be3fc92e0daaaf0c1931723a2aa34df
T K <kytomaa@gmail.com>
2015-02-04 19:46:18 -0800

    Hack FT runner to be able to test staging

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -3,9 +3,25 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 # import unittest
 import time
+import sys
 
 
 class NewVisitorTest(StaticLiveServerTestCase):
+
+	@classmethod
+	def setUpClass(cls):
+		for arg in sys.argv:
+			if 'liveserver' in arg:
+				cls.server_url = 'http://' + arg.split('=')[1]
+				return
+		super().setUpClass()
+		cls.server_url = cls.live_server_url
+
+	@classmethod
+	def tearDownClass(cls):
+
+		if cls.server_url == cls.live_server_url:
+			super().tearDownClass()
 
 	def setUp(self):
 		self.browser = webdriver.Firefox()
@@ -23,7 +39,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
 		# cool new online to-do app
 		# self.browser.get('http://localhost:8000')
-		self.browser.get(self.live_server_url)
+		self.browser.get(self.server_url)
 
 		# title and headers mentions to-do list
 		self.assertIn('To-Do', self.browser.title)
@@ -64,7 +80,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 		self.browser = webdriver.Firefox()
 
 		# Francis comes along....
-		self.browser.get(self.live_server_url)
+		self.browser.get(self.server_url)
 		page_text = self.browser.find_element_by_tag_name('body').text
 		self.assertNotIn('Buy peacock feathers', page_text)
 		self.assertNotIn('make a fly', page_text)
@@ -88,7 +104,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 	def test_layout_and_styling(self):
 
 		# edith gos to homepage
-		self.browser.get(self.live_server_url)
+		self.browser.get(self.server_url)
 		self.browser.set_window_size(1024, 768)
 
 		# she notices input bx is nicely centered
