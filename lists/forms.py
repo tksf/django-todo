e6eb50a73543769ac250b6dedfb0208bb9009981
T K <kytomaa@gmail.com>
2015-02-08 17:10:13 -0800

    finished form validation magic

--- lists/forms.py
+++ lists/forms.py
@@ -38,6 +38,8 @@ class ExistingListItemForm(ItemForm):
 			e.error_dict = {'text' : [DUPLICATE_ITEM_ERROR]}
 			self._update_errors(e)
 
+	def save(self):
+		return forms.models.ModelForm.save(self)
 
 
 	# item_text = forms.CharField(
--- lists/tests/test_forms.py
+++ lists/tests/test_forms.py
@@ -60,7 +60,11 @@ class ExistingListItemFormTest(TestCase):
 			form.errors['text'], [DUPLICATE_ITEM_ERROR]
 		)
 
-
+	def test_form_save(self):
+		list_ = List.objects.create()
+		form = ExistingListItemForm(for_list=list_, data={'text' : 'hi'})
+		new_item = form.save()
+		self.assertEqual(new_item, Item.objects.all()[0])
 
 
 
